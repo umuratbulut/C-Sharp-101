55bab211e99c17dbffece497ad8bb477b757d290
Murat BULUT <83401947+umuratbulut@users.noreply.github.com>
2021-12-20 12:53:29 +0300

    If, Else,Ternary project added.

---
+++ If-Else-Ternary/If-Else-Ternary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace If_Else_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+                Console.WriteLine("Girdiğiniz sayı 0'dan büyük.");
+            else
+                Console.WriteLine("Girdiğiniz sayı 0'dan küçük.");
+            // Ternary
+            int c = a > 0 ? a : 0;
+            Console.WriteLine(c);
+        }
+    }
+}
