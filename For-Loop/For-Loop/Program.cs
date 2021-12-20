aefeed1223a0fad7b48b784116643ed4941ec1e4
Murat BULUT <83401947+umuratbulut@users.noreply.github.com>
2021-12-20 13:39:46 +0300

    For Loop project added.

---
+++ For-Loop/For-Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write((i + 1) + ".sayı giriniz:");
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(dizi);
+            Console.WriteLine("-------------");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
