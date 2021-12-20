b1b491cc4fb35af75c960ccd7704b128498589cc
Murat BULUT <83401947+umuratbulut@users.noreply.github.com>
2021-12-20 13:33:34 +0300

    Switch case project added.

---
+++ Switch-Case/Switch-Case/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1 - 4 arasında bir sayı giriniz:");
+            int a = int.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Mclaren");
+                    break;
+                case 2:
+                    Console.WriteLine("Bugatti");
+                    break;
+                case 3:
+                    Console.WriteLine("Koeingsegg");
+                    break;
+                case 4:
+                    Console.WriteLine("Ferrari");
+                    break;
+                default:
+                    Console.WriteLine("Girdiğiniz sayı hatalı!");
+                    break;
+            }
+
+        }
+    }
+}
