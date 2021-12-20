d17d62dadc23beaff05f9dd2c86fba9099fda894
Murat BULUT <83401947+umuratbulut@users.noreply.github.com>
2021-12-20 14:39:17 +0300

    While, Foreach project added.

---
+++ While-Foreach/While-Foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+
+            // Foreach
+            int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+            int toplam = 0;
+
+            foreach (int i in sayiDizisi)
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine(toplam);
+        }
+    }
+}
